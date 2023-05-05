28fd5f2daa19d8b7eff1ee4c9ac1e1338bcc1246
DIEGO VINICIUS DE SOUSA AZEVEDO <diego.v.azevedo@una.br>
2023-05-04 21:35:40 -0300

    first commit

--- src/App.java
+++ src/App.java
@@ -5,13 +5,17 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         System.out.println("Digite um número inteiro: ");
-       int a = input.nextInt();
+       int Inteiro1 = input.nextInt();
         System.out.println("Digite outro número inteiro: ");
-       int b = input.nextInt();
+       int Inteiro2 = input.nextInt();
         System.out.println("Digite um núemro real: ");
-       Double c = input.nextDouble();
-       Double Total = a + b + c;
-       System.out.println("Valor total: " + Total);
+       Double Inteiro3 = input.nextDouble();
+       Double a = (2 * Inteiro1) * (Inteiro2 / 2.0);
+       Double b = (3 * Inteiro1) * Inteiro3;
+       Double c = Math.pow(Inteiro3, 3);
+       System.out.println("Resultado de a = " + a);
+       System.out.println("Resultado de b = " + b);
+       System.out.println("Resultado de c = " + c);
        input.close();
 
     }
